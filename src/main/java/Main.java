1c57c66e58130edef24551815106452fe59d4027
dmitry <my@my.ru>
2018-04-03 00:29:29 +0300

    - 7430 - huge performance optimizations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005,quiet=y -jar /Users/fox/projects/miniaicup2/ms.jar true
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005,quiet=y -jar /Users/fox/projects/miniaicup2/ms.jar true
 
+﻿java -jar /Users/fox/projects/miniaicup2/7430.jar
 ﻿java -jar /Users/fox/projects/miniaicup2/7412.jar
 ﻿java -jar /Users/fox/projects/miniaicup2/6835.jar
 ﻿java -jar /Users/fox/projects/miniaicup2/6833.jar - more fear
