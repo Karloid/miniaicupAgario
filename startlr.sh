a8f13c4db0211ade9be5139b5f0c3373198afeb6
dmitry <my@my.ru>
2018-04-19 00:43:31 +0300

    - return back intense tracing

--- startlr.sh
+++ startlr.sh
@@ -2,4 +2,4 @@
 export RESP_TIMEOUT="3600" &&
 export GAME_WIDTH="990" &&
 export GAME_HEIGHT="990" &&
-export TICK_MS="2" && open ~/projects/miniaicups/agario/local_runner/build/local_runner.app
+export TICK_MS="10" && open ~/projects/miniaicups/agario/local_runner/build/local_runner.app
